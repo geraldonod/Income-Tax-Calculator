80fd8d17d025e1d7b1cb1f1346845eb51dd8b7c9
Gerald <geraldonod@gmail.com>
2024-01-26 12:52:11 +0800

    ask user for income value

--- Income tax calculator.py
+++ Income tax calculator.py
@@ -1,4 +1,8 @@
 #ask user for income value
+user_income = float(input("Enter the taxable income: "))
+tax_payable = 0
+
+print("Given income:", user_income)
 
 #if-else statements to identify interest rate
 
