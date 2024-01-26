c43db0668b5680d8227de94389e7dacbfd834c3f
Gerald <geraldonod@gmail.com>
2024-01-26 13:04:37 +0800

    if-else statements

--- Income tax calculator.py
+++ Income tax calculator.py
@@ -1,9 +1,17 @@
 #ask user for income value
 user_income = float(input("Enter the taxable income: "))
-tax_payable = 0
+user_tax_payable = 0
 
 print("Given income:", user_income)
 
 #if-else statements to identify interest rate
+if user_income <= 10000:
+    user_tax_payable = 0
+elif user_income <= 20000:
+    x = user_income - 10000
+    user_tax_payable = x * 10 / 100
+else:
+    user_tax_payable = 10000 * 10 / 100
+    user_tax_payable += (user_income - 20000) * 20 / 100
 
 #print result
